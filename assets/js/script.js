ff0efbede00ea18515f7c476410b4172209c7b09
Pouriya Amjadzadeh <p.amjadzadeh@gmail.com>
2021-10-30 15:12:47 +0330

    Updated Core

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,11 @@ requirejs.config({
 	baseUrl: rqjs.getAttribute('src').replace('require.js',''),
 	paths: {
 		mmenu: 'mmenu-light',
-		bootstrap: 'bootstrap.bundle.min',
-	},
-	shim : {
-		'bootstrap' : { 'deps' :['jquery'] }
-	},
+	}
 });
-
-if (typeof jQuery === 'function') define('jquery', function () {
+if (typeof jQuery === 'function') define('jQuery', function () {
 	return jQuery;
 });
-
 const imageObserver = new IntersectionObserver((entries, imgObserver) => {
 	entries.forEach(function(entry){
 		if (entry.isIntersecting){
@@ -24,59 +18,41 @@ const imageObserver = new IntersectionObserver((entries, imgObserver) => {
 	});
 });
 
-requirejs(['bootstrap'], function($){
-	jQuery(document).ready(function($){
-		"use strict";
+jQuery(document).ready(function($){
+	"use strict";
 
-		if(document.getElementById('mmenu')){
-			requirejs(['mmenu'], function(){
-				const menu = new MmenuLight(document.getElementById('mmenu')), drawer = menu.offcanvas({position: ((document.dir == 'rtl') ? 'right' : 'left')});
-				menu.navigation({title: $('#mmenu').data('mm-spn-title')});
-				$('[href="#mmenu"]').click(function (e) { 
-					e.preventDefault();
-					drawer.open();
-				});
-			});
+	function init_scripts(){
+		const lazyImages = document.querySelectorAll('[data-src]');
+		for (var i = 0; i < lazyImages.length; i++) {
+			imageObserver.observe(lazyImages[i]);
 		}
-		
-		function initjsFuncs(){
-			const lazyImages = document.querySelectorAll('[data-src]');
-			for (var i = 0; i < lazyImages.length; i++) {
-				imageObserver.observe(lazyImages[i]);
-			}
-			if(document.getElementsByClassName('splide')){
-				requirejs(['splide'], function(){
-					const splides = document.getElementsByClassName('splide');
-					for ( var i = 0; i < splides.length; i++ ) {
-						new Splide(splides[i]).mount();
-					}
-				});
-			}
-			$('[data-bs-toggle="tooltip"]').tooltip();
-		}
-		
-		initjsFuncs();
 
-		/*
-		Ajax Template
-		$.ajax({
-			type : 'POST',
-			url : rqjs.getAttribute('data-ajax'),
-			data : {
-				action : 'actionname',
-				inputs : $(this).serialize()
-			},
-			beforeSend: function() {
-				...
-			},
-			success : function(resp) {
-				...
-			},
-			complete: function(){
-				initAjax();
+		requirejs(['bootstrap'], function(bootstrap){
+			const tooltipList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+			for ( var i = 0; i < tooltipList.length; i++ ) {
+				new bootstrap.Tooltip(tooltipList[i]);
 			}
 		});
-		*/
 
-	});
+		if(document.getElementsByClassName('splide')){
+			requirejs(['splide'], function( Splide ){
+				const splides = document.getElementsByClassName('splide');
+				for ( var i = 0; i < splides.length; i++ ) {
+					new Splide(splides[i]).mount();
+				}
+			});
+		}
+	}
+	init_scripts();
+
+	if(document.getElementById('mmenu')){
+		requirejs(['mmenu'], function(){
+			const menu = new MmenuLight(document.getElementById('mmenu')), drawer = menu.offcanvas({position: ((document.dir == 'rtl') ? 'right' : 'left')});
+			menu.navigation({title: $('#mmenu').data('mm-spn-title')});
+			$('[href="#mmenu"]').click(function (e) { 
+				e.preventDefault();
+				drawer.open();
+			});
+		});
+	}
 });
