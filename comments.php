c73fbb544a247e601cf99174cc9763b2b4b5ecda
Pouriya Amjadzadeh <p.amjadzadeh@gmail.com>
2023-10-23 18:17:13 +0330

    Added 404 & Page php

--- comments.php
+++ comments.php
@@ -1,8 +1,15 @@
 <?php
+/**
+ * The template for displaying comments.
+ *
+ * The area of the page that contains both current comments
+ * and the comment form.
+ *
+ */
 
 defined( 'ABSPATH' ) || exit;
 
-if( ! comments_open() || post_password_required() ) return;
+if( ( !comments_open() && !have_comments() ) || post_password_required() ) return;
 ?>
 
 <div id="comments">
