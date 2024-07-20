73aef772a53afacb44e5a093c40f04d68ff9255d
Pouriya Amjadzadeh <p.amjadzadeh@gmail.com>
2024-07-20 14:00:33 +0330

    Js updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,72 @@
 const themejs = document.getElementById('themejs'), ajaxURL = themejs.dataset.ajax;
 
-const validationForms = document.querySelectorAll('.form-validation');
-Array.from(validationForms).forEach(function(form) {
-	form.addEventListener('submit', function(e){
-		if( !form.checkValidity() ) {
-			e.preventDefault();
-			e.stopPropagation();
-		}
-		form.classList.add('was-validated')
-	}, false);
-	const inputs = form.querySelectorAll('input, textarea, select');
-	Array.from(inputs).forEach(function(input) {
-		input.oninvalid = function(e) {
-			e.target.setCustomValidity('');
-			if( !e.target.validity.valid && e.target.title.length > 0 ) {
-				e.target.setCustomValidity( e.target.title );
-			}
-		}
-		input.oninput = function(e) {
-			e.target.setCustomValidity('');
-		}
-	});
-});
+//Form JS Validation
+// const validationForms = document.querySelectorAll('.form-validation');
+// Array.from(validationForms).forEach(function(form) {
+// 	form.addEventListener('submit', function(e){
+// 		if( !form.checkValidity() ) {
+// 			e.preventDefault();
+// 			e.stopPropagation();
+// 		}
+// 		form.classList.add('was-validated')
+// 	}, false);
+// 	const inputs = form.querySelectorAll('input, textarea, select');
+// 	Array.from(inputs).forEach(function(input) {
+// 		input.oninvalid = function(e) {
+// 			e.target.setCustomValidity('');
+// 			if( !e.target.validity.valid && e.target.title.length > 0 ) {
+// 				e.target.setCustomValidity( e.target.title );
+// 			}
+// 		}
+// 		input.oninput = function(e) {
+// 			e.target.setCustomValidity('');
+// 		}
+// 	});
+// });
 
-if( typeof Splide !== 'undefined' && document.querySelector('.splide') ){
-	document.addEventListener('DOMContentLoaded', function(){
-		const splides = document.querySelectorAll( '.splide' );
-		for ( var i = 0; i < splides.length; i++ ) {
-			if( typeof( splides[i].dataset.splide ) === "undefined" || splides[i].dataset.splide === null ) return false;
-			new Splide( splides[i] ).mount();
-		}
-	});
-}
+
+//Splidejs
+// if( typeof Splide !== 'undefined' && document.querySelector('.splide') ){
+// 	document.addEventListener('DOMContentLoaded', function(){
+// 		const splides = document.querySelectorAll( '.splide' );
+// 		for ( var i = 0; i < splides.length; i++ ) {
+// 			if( typeof( splides[i].dataset.splide ) === "undefined" || splides[i].dataset.splide === null ) return false;
+// 			new Splide( splides[i] ).mount();
+// 		}
+// 	});
+// }
+
+//Woocommerce Notices
+// document.addEventListener('DOMContentLoaded', function() {
+// 	var observer = new MutationObserver(function(mutationsList, observer) {
+// 		mutationsList.forEach(function(mutation){
+// 			if (mutation.type === 'childList' && document.querySelector('.woocommerce-message') ) {
+// 				let wcAlerts = document.querySelectorAll('.woocommerce-message'), timehide = 3000;
+// 				wcAlerts.forEach(function(alert, index) {
+// 					setTimeout(function() {
+// 						alert.style.opacity = '0';
+// 						setTimeout(function(){
+// 							alert.remove();
+// 						}, 750);
+// 					}, timehide);
+// 					timehide += 1500;
+// 				});
+// 			}
+// 		});
+// 	});
+// 	observer.observe(document.body, {childList: true, subtree: true});
+// });
 
 if( typeof jQuery !== 'undefined' ){
 	jQuery(document).ready(function($) {
-		$('.collapse-menu li').click(function (e) {
-			if(this != e.target) return;
-			e.preventDefault();
-			$(this).toggleClass('item-opened').find('> ul').slideToggle();
-		});
-	
+
+		//Offcanvas mmenu
+		// $('.collapse-menu li').click(function (e) {
+		// 	if(this != e.target) return;
+		// 	e.preventDefault();
+		// 	$(this).toggleClass('item-opened').find('> ul').slideToggle();
+		// });
+
 		//Jquery comes here...
-		
-		//Woocomemrce JS
-		function wcMessagesHandler(){
-			if( document.querySelector('.wc-alert') ){
-				var wcAlerts = document.querySelectorAll('.wc-alert'), timehide = 6000;
-				for (let i = 0; i < wcAlerts.length; i++) {
-					const selected = wcAlerts[ i ], bsAlert = new bootstrap.Alert( selected );
-					$(selected).find('.progress').animate({width: '0%'}, timehide);
-					setTimeout(function(){
-						bsAlert.close();
-					}, timehide);
-					timehide += 1500;
-				}
-			}
-		}
-		
-		wcMessagesHandler();
-		$(document).bind('DOMNodeInserted', function(e) {
-			wcMessagesHandler();
-		});
 	});
 }
