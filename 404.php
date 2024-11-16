641ff7d38d42a1fe588476efc16b74743f8a36cc
pamjadz <p.amjadzadeh@gmail.com>
2024-11-16 14:25:13 +0330

    Removed everything

--- 404.php
+++
@@ -1,4 +0,0 @@
-<?php
-/**
- * The template for displaying 404 pages (not found).
- */
--- single.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- */
-
-defined('ABSPATH') || exit;
